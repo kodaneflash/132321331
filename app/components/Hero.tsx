e42648f3156b32693bb2f5b3a49ff0c932298317
kodaneflash <jamesonmarcher@gmail.com>
2024-04-14 13:02:37 -0400

    modified:   app/components/Hero.tsx modified:   app/components/ImageGallery.tsx 	modified:   app/components/Newest.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,11 +17,12 @@ export default async function Hero() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+          IDEmpire - Your Trusted Vendor for Premium Fake IDs 
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.
-            We are the best so come and shop with us.
+          Shop for premium fake IDs that feature scannable barcodes, microprint,
+          OVI ink, OVD holograms, laser engraving & more. Available for nearly all states with
+          fast shipping and reliable support.
           </p>
         </div>
 
--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -18,26 +18,26 @@ export default function ImageGallery({ images }: iAppProps) {
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
         {images.map((image: any, idx: any) => (
-          <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
+          <div key={idx} className="rounded-lg bg-gray-100">
             <Image
               src={urlFor(image).url()}
-              width={200}
-              height={200}
-              alt="photo"
-              className="h-full w-full object-cover object-center cursor-pointer"
+              width={500}
+              height={500}
+              alt="IDEmpirePremium Fake ID"
+              className="h-full w-full object-contain object-center cursor-pointer"
               onClick={() => handleSmallImageClick(image)}
             />
           </div>
         ))}
       </div>
 
-      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+      <div className="relative  rounded-lg bg-gray-100 lg:col-span-4">
         <Image
           src={urlFor(bigImage).url()}
-          alt="Photo"
+          alt="IDEmpirePremium Fake ID"
           width={500}
           height={500}
-          className="h-full w-full object-cover object-center"
+          className="h-full w-full object-contain object-center"
         />
 
         <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">
