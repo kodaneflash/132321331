7c9350acea8b52ecac5cf49e23ac529764bba7d9
James A <jamesonmarcher@gmail.com>
2024-04-14 23:10:05 -0400

    Apply gradient effect to 'ID Empire' text in Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -20,8 +20,8 @@ export default function Navbar() {
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
-          <h1 className="text-2xl md:text-4xl font-bold">
-            ID<span className="text-primary">Empire</span>
+            <h1 className="text-2xl md:text-4xl font-bold">
+            <span className="gradient-text">ID</span><span className="gradient-text">Empire</span>
           </h1>
         </Link>
 
