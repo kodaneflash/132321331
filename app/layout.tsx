4112742e35e19ddcc3a8d38111a3a756fc269afb
kodaneflash <jamesonmarcher@gmail.com>
2024-04-15 17:32:23 -0400

    eeeee

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,11 +50,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+        <meta property="og:url" content="https://idempire.us" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="IDEmpire" />
+        <meta property="og:description" content="#1 Trusted Vendor for Premium Fake IDs." />
+        <meta property="og:image" content="https://www.idempire.us/og-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="idempire.us" />
+        <meta property="twitter:url" content="https://idempire.us" />
+        <meta name="twitter:title" content="IDEmpire" />
+        <meta name="twitter:description" content="#1 Trusted Vendor for Premium Fake IDs." />
+        <meta name="twitter:image" content="https://www.idempire.us/og-image.jpg" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="canonical" href="https://www.idempire.us" />      </Head>
       <body className={inter.className}>
         <CartProvider>
           <Navbar />
@@ -62,6 +75,6 @@ export default function RootLayout({
           {children}
         </CartProvider>
       </body>
-    </html>
+    </>
   );
 }
