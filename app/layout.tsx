54747b61c5fe651002c62f2ac99edc8f94682bb5
kodaneflash <jamesonmarcher@gmail.com>
2024-04-15 17:20:20 -0400

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,44 @@ import "./globals.css";
 import CartProvider from "./components/Providers";
 import Navbar from "./components/Navbar";
 import ShoppingCartModal from "./components/ShoppingCartModal";
-import Head from 'next/head';
-
+import Head from 'next/head'; 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IDEmpire - #1 Trusted Vendor for Premium Fake IDs",
-  description: "Shop for premium fake IDs that feature scannable barcodes, microprint, OVI ink, OVD holograms, laser engraving & more. Available for nearly all states with fast shipping and reliable support. ",
+  title: "IDEmpire",
+  description: "#1 Trusted Vendor for Premium Fake IDs",
+  openGraph: {
+    title: "IDEmpire",
+    description: "#1 Trusted Vendor for Premium Fake IDs.",
+    images: [
+      {
+        url: "https://www.idempire.us/og-image.jpg",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    url: "https://www.idempire.us",
+    type: "website",
+    siteName: "IDEmpire",
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@idempire",
+    title: "IDEmpire",
+    description: "#1 Trusted Vendor for Premium Fake IDs.",
+    images: ["https://www.idempire.us/twitter-image.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  alternates: {
+    canonical: "https://www.idempire.us",
+    languages: {
+      "en-US": "https://idempire.us",
+      es: "https://www.IDEmpire.us/es",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -23,14 +54,6 @@ export default function RootLayout({
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Shop for premium fake IDs with scannable barcodes, microprint, and more. Available for nearly all states with fast shipping and reliable support." />
-        <link rel="icon" href="/favicon.ico" />
-        <title>IDEmpire - #1 Trusted Vendor for Premium Fake IDs</title>
-        <meta property="og:title" content="IDEmpire - #1 Trusted Vendor for Premium Fake IDs" />
-        <meta property="og:description" content="Shop for premium fake IDs that feature scannable barcodes, microprint, OVI ink, OVD holograms, laser engraving & more. Available for nearly all states with fast shipping and reliable support." />
-        <meta property="og:image" content="/og-image.png" />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
       </Head>
       <body className={inter.className}>
         <CartProvider>
