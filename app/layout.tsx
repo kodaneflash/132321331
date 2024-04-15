2ad402e6b282fb7c676d573b0f0a5e00e2320ab8
kodaneflash <jamesonmarcher@gmail.com>
2024-04-15 17:50:25 -0400

    sssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import "./globals.css";
 import CartProvider from "./components/Providers";
 import Navbar from "./components/Navbar";
 import ShoppingCartModal from "./components/ShoppingCartModal";
-import Head from 'next/head'; 
+import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata: Metadata = {
   title: "IDEmpire",
@@ -16,12 +15,12 @@ export const metadata: Metadata = {
     description: "#1 Trusted Vendor for Premium Fake IDs.",
     images: [
       {
-        url: process.env.NEXT_PUBLIC_BASE_URL + "/og-image.jpg", // Directly use process.env.NEXT_PUBLIC_BASE_URL
+        url: "https://www.idempire.us/og-image.jpg",
         width: 1920,
         height: 1080,
       },
     ],
-    url: process.env.NEXT_PUBLIC_BASE_URL, // Directly use process.env.NEXT_PUBLIC_BASE_URL
+    url: "https://www.idempire.us",
     type: "website",
     siteName: "IDEmpire",
   },
@@ -30,17 +29,17 @@ export const metadata: Metadata = {
     creator: "@idempire",
     title: "IDEmpire",
     description: "#1 Trusted Vendor for Premium Fake IDs.",
-    images: [process.env.NEXT_PUBLIC_BASE_URL + "/twitter-image.jpg"], // Directly use process.env.NEXT_PUBLIC_BASE_URL
+    images: ["https://www.idempire.us/twitter-image.jpg"],
   },
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
   alternates: {
-    canonical: process.env.NEXT_PUBLIC_BASE_URL, // Directly use process.env.NEXT_PUBLIC_BASE_URL
+    canonical: "https://www.idempire.us",
     languages: {
-      "en-US": process.env.NEXT_PUBLIC_BASE_URL, // Directly use process.env.NEXT_PUBLIC_BASE_URL
-      es: process.env.NEXT_PUBLIC_BASE_URL + "/es", // Directly use process.env.NEXT_PUBLIC_BASE_URL
+      "en-US": "https://idempire.us",
+      es: "https://www.IDEmpire.us/es",
     },
   },
 };
@@ -51,22 +50,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
+        <meta property="og:url" content="https://idempire.us" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="IDEmpire" />
         <meta property="og:description" content="#1 Trusted Vendor for Premium Fake IDs." />
-        <meta property="og:image" content={process.env.NEXT_PUBLIC_BASE_URL + "/og-image.jpg"} />
+        <meta property="og:image" content="https://www.idempire.us/og-image.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="idempire.us" />
+        <meta property="twitter:url" content="https://idempire.us" />
         <meta name="twitter:title" content="IDEmpire" />
         <meta name="twitter:description" content="#1 Trusted Vendor for Premium Fake IDs." />
-        <meta name="twitter:image" content={process.env.NEXT_PUBLIC_BASE_URL + "/twitter-image.jpg"} />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL} />
+        <meta name="twitter:image" content="https://www.idempire.us/og-image.jpg" />
       </Head>
       <body className={inter.className}>
         <CartProvider>
@@ -75,6 +73,6 @@ export default function RootLayout({
           {children}
         </CartProvider>
       </body>
-    </>
+    </html>
   );
 }
